cb67a81f86f70f87fac5c92d968d3308bd5cc381
Mehmet Ali Ayan <maliayan34@gmail.com>
2018-07-20 19:21:09 +0300

    Adding alt attributes for images

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -88,6 +88,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
+  image.setAttribute("alt", "Restaurant image of " + restaurant.name);
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
